06a48e35ab7c99059ca861e18c861b8ded21e151
Andre Geldenhuis <andre.geldenhuis@vuw.ac.nz>
2014-11-26 14:47:04 +1300

    added method to easily debug with ipython --pdb --> cmdline arg for manage.py

--- blogaggregator/manage.py
+++ blogaggregator/manage.py
@@ -37,9 +37,25 @@ def test():
 def runwaitress():
     serve(app,host="0.0.0.0", port=5000)
 
+
+@manager.command
+def cmdline():
+    """
+    A small function  to allow testting the sqalchemy phrasing in ipython
+    functions similarly to a view
+    """
+    from flask import Blueprint, render_template, request, flash, redirect, url_for, session
+    from flask.ext.login import login_required
+    from flask.ext.login import current_user
+    from blogaggregator.user.forms import User
+    1/0
+
+
 manager.add_command('server', Server())
 manager.add_command('shell', Shell(make_context=_make_context))
 manager.add_command('db', MigrateCommand)
+
+
 
 if __name__ == '__main__':
     manager.run()
