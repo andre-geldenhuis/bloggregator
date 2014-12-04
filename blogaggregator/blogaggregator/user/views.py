8edb137dd54e36ca335ec075897ae6509c574420
Andre Geldenhuis <andre.geldenhuis@vuw.ac.nz>
2014-12-04 17:34:10 +1300

    Simple comment editing now in place, this really needs to be cleaned up for code reuse and readability!

--- blogaggregator/blogaggregator/user/views.py
+++ blogaggregator/blogaggregator/user/views.py
@@ -2,7 +2,7 @@
 from flask import Blueprint, render_template, request, flash, url_for, redirect
 from flask.ext.login import login_required
 from flask.ext.login import current_user
-from sqlalchemy import asc
+from sqlalchemy import asc, desc
 
 from blogaggregator.user.forms import PostForm
 from blogaggregator.user.forms import CommentForm
@@ -50,15 +50,28 @@ def addpost():
     
     return render_template("users/addpost.html", form=form)
 
-@blueprint.route("/posts/<username>/<postid>/", methods=["GET","POST"])
-@login_required
+#TODO move this into the public views!
+@blueprint.route("/posts/<username>/<postid>/", methods=["GET"])
 def comment(username,postid):
+    #~ form = CommentForm(request.form)
+    allowedit = False # by default, no edits
+    comments_all=db.session.query(Comment).filter(Comment.post_id==postid).order_by(asc(Comment.created_at)).all()
+    if comments_all == None:
+        comments_all=[{'content':'No comments yet :('}]
+    else:  #check if the last post is byt he current user (for comment edits)
+        last_comment_userid=comments_all[-1].user.id
+        if last_comment_userid == current_user.id:
+            allowedit = True
+    return render_template('users/comments.html',username=username,postid=postid,comments_all=comments_all, allowedit=allowedit)
+    
+
+# TODO fix, this doesn't also display the commments as it should    
+@blueprint.route("/posts/<username>/<postid>/comment", methods=["POST","GET"])
+@login_required
+def makecomment(username,postid):
     form = CommentForm(request.form)
     if request.method == "GET":
-        comments_all=db.session.query(Comment).filter(Comment.post_id==postid).order_by(asc(Comment.created_at)).all()
-        if comments_all == None:
-            comments_all=[{'content':'No comments yet :('}]
-    
+        pass
     if request.method == "POST":
         if form.validate_on_submit:
             comment = form.comment.data
@@ -75,4 +88,41 @@ def comment(username,postid):
             return redirect(redirect_url)
         else:
             flash_errors(form)
-    return render_template('users/comments.html',form=form,comments_all=comments_all)
+            redirect_url =  url_for('user.comment',username=username, postid=postid)
+            return redirect(redirect_url)
+    return render_template('users/newcomment.html',form=form)
+
+# TODO modify this for code reuse!  really similar to makecomment
+@blueprint.route("/posts/<username>/<postid>/editcomment", methods=["POST","GET"])
+@login_required
+def editcomment(username,postid):
+    # Check if user is still allowed to make edit
+    allowedit = False # by default, no edits
+    comment_last=db.session.query(Comment).filter(Comment.post_id==postid).order_by(desc(Comment.created_at)).first()
+    if comment_last.user_id == current_user.id :
+        old_content=comment_last.content
+        row={'comment':old_content}
+        form = CommentForm(**row)
+        if request.method == "GET":
+            pass
+            
+        if request.method == "POST":
+            if form.validate_on_submit:
+                commentedit = form.comment.data
+                comment_last.content=commentedit
+                db.session.commit()
+                flash("Comment edited.", 'success')
+                redirect_url =  url_for('user.comment',username=username, postid=postid)
+                return redirect(redirect_url)
+            else:
+                flash_errors(form)
+                redirect_url =  url_for('user.comment',username=username, postid=postid)
+                return redirect(redirect_url)
+        return render_template('users/editcomment.html',form=form)
+    
+    else:
+        flash("Sorry you cannot edit this post","warning")
+        redirect_url =  url_for('user.comment',username=username, postid=postid)
+        
+    
+    
